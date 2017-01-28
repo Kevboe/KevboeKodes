3281c5bcb4a94fcf26571ef40666e1e5df46de99
Kevin <thekev33@gmail.com>
2017-01-28 11:50:37 -0800

    First upload of count the days - still needs work

---
+++ CMSI-186_Projects/Homework01/countTheDays.java
@@ -0,0 +1,24 @@
+/*
+ *  File name     :  CountTheDays.java
+ *  Purpose       :  Creating countTheDays.java program
+ *  Author        :  Kevin Patterson
+ *  Date          :  1/26/17
+ *  Description   :  Creation of the countTheDays class. Calls upon methods in the CalendarStuff
+ *                   class to run.
+ *  Notes         :  None
+ *  Warnings      :  None
+ *  Exceptions    :  None
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  Revision History
+ *  ----------------
+ *            Rev      Date     Modified by:     Reason for change/modification
+ *           -----  ----------  ------------     -----------------------------------------------------------
+ *  @version 1.0.0  2017-01-02  Kevin Patterson  Initial writing
+ */
+
+public class countTheDays{
+  public static void main (String [] args){
+  private long d1, d2, m1, m2, y1, y2;
+  
+  }
+}
